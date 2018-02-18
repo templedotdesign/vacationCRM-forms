4d02331c0d99854242e86eaf4693f60a211f5bb4
Ryan Temple <temple.design.me@gmail.com>
2018-02-18 14:49:34 -0500

    initial commit

---
+++ app/src/components/app/app.js
@@ -0,0 +1,12 @@
+//Core
+import React from 'react';
+
+const app = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default app;
---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+//Core
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import registerServiceWorker from './registerServiceWorker';
+
+//Components
+import App from './components/app/app';
+
+//CSS
+import './index.css';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+//registerServiceWorker();
